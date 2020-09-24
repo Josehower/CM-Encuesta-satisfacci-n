d8df4fa98ed0ae70e7e2a5bfac19b330faf12560
Josehower <josehowerupn@gmail.com>
2020-09-24 21:03:15 +0200

    netlify attribute

--- src/Forma.js
+++ src/Forma.js
@@ -3,7 +3,7 @@ import RadialInput from './RadialInput';
 
 const Forma = () => {
   return (
-    <form action="submit">
+    <form name="Encuesta" method="POST" data-netlify="true">
       <p>Califíque su nivel de satisfacción en los siguientes aspectos:</p>
       <RadialInput
         id={'expectativas'}
